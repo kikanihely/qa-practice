f936520965f0c6ea891ceb191f4cd61a25fc086e
Kikani Hely <151033985+kikanihely@users.noreply.github.com>
2026-06-01 15:53:57 +0530

    Add files via upload

---
+++ tests/xpath-filtering-locators.spec.ts
@@ -0,0 +1,21 @@
+import {test} from "@playwright/test";
+
+test("XPath and Filtering Locators", async ({page}) => {
+
+    //XPath locators
+    await page.goto("https://eventhub.rahulshettyacademy.com")
+    
+    //basic xpath
+    await page.locator("//input[@id='email']").fill("hely@gmail.com")
+    
+    //parent to child
+    await page.locator("//form//div//input[@type='password']").fill("Hely@3014")
+
+    //by position
+    await page.locator("(//button)[1]").click();
+
+    //Filtering locators
+    await page.locator('#event-card').filter({hasText: "Dilli Diwali Mela"}).getByRole("link",{name: "Book Now"}).click();
+
+
+})
