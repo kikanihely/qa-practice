9bd9bd6ac2b4df169f6873a2d55f657a157817f2
Kikani Hely <helykikani2020@gmail.com>
2026-06-03 10:31:30 +0530

    Pop ups/Multi added

--- tests/multiple-window.spec.ts
+++ tests/multiple-window.spec.ts
@@ -2,7 +2,7 @@ import {test} from '@playwright/test';
 
 test("Multiple Windows", async ({page}) => {
     await page.goto("https://www.fabindia.com/")
-    await page.getByAltText("Men Kurtas").first().click();
+    await page.getByAltText("Men Kurtas").click();
 
     const page1Promise= page.waitForEvent("popup");
     await page.getByAltText("Grey Cotton Stripe Short Kurta").click();
