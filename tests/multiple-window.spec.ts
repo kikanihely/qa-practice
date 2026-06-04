f94f2750269f951212d1b5cd6b3fc9275b659ea9
Kikani Hely <helykikani2020@gmail.com>
2026-06-04 15:57:54 +0530

    Commited

--- tests/multiple-window.spec.ts
+++ tests/multiple-window.spec.ts
@@ -1,12 +1,22 @@
-import {test} from '@playwright/test';
+import { test } from '@playwright/test';
 
-test("Multiple Windows", async ({page}) => {
-    await page.goto("https://www.fabindia.com/")
-    // await page.getByAltText("Men Kurtas").nth(1).click();
+test("Multiple Windows using Browser Fixture", async ({ browser }) => {
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto("https://www.fabindia.com/");
+
     await page.locator('img[alt="Men Kurtas"][loading="lazy"]').first().click();
-    const page1Promise= page.waitForEvent("popup");
-    await page.getByRole("img", {name: "Grey Cotton Stripe Short Kurta"}).click();
-    const page1 = await page1Promise;
-    await page1.waitForLoadState();
-    await page1.getByRole("button", {name: "Add to Cart"}).click();
-})
+
+    const [newPage] = await Promise.all([
+        page.waitForEvent('popup'),
+        page.getByRole("img", { name: "Grey Cotton Stripe Short Kurta" }).click()
+    ]);
+
+    await newPage.waitForLoadState();
+
+    await newPage.getByRole("button", { name: "Add to Cart" }).click();
+
+    await context.close();
+});
