51b46ed2feba6cbd6d33e52408fb9a03e97ca90c
Kikani Hely <helykikani2020@gmail.com>
2026-06-04 10:31:51 +0530

    Screenshot added

--- tests/screenshot.spec.ts
+++ tests/screenshot.spec.ts
@@ -5,18 +5,18 @@ test("Screenshot", async ({page}) => {
 
     //full page screenshot
     await page.screenshot({
-        path: "screenshot.png",
+        path: "screenshot/screenshot.png",
         fullPage: true
     })
 
     //viewport screenshot
     await page.screenshot({
-        path: "viewport.png",
+        path: "screenshot/viewport.png",
         fullPage: false
     })
 
     //screenshot of specific element
     await page.locator("#email").screenshot({  
-        path: "emailelement.png"
+        path: "screenshot/emailelement.png"
     })
 })
