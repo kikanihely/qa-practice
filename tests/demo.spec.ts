918ff093d49bf06cc0aa6403233c9930c520ca41
Kikani Hely <helykikani2020@gmail.com>
2026-06-05 16:05:40 +0530

    Commited

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -2,6 +2,9 @@ import {test} from '@playwright/test';
 
 test("Demo Test", async ({page}) => {
     
-    await page.goto("https://www.snapdeal.com/");
-    
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    await page.getByPlaceholder("Username").fill("Admin");  
+    await page.getByPlaceholder("Password").fill("admin123");
+    await page.locator(".orangehrm-login-button").click();
+
 });
