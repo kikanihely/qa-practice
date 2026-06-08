10a8195e3e36bea8cc79155b283f58abfc4b474d
Kikani Hely <helykikani2020@gmail.com>
2026-06-08 15:53:21 +0530

    Add Employee Flow, Dashboard and Login  added

---
+++ pages/DashboardPage.ts
@@ -0,0 +1,40 @@
+import {test, expect} from "@playwright/test";
+import {Locator, Page} from "@playwright/test";
+
+export class DashboardPage {
+    timeAtWorkLocator: Locator;
+    myActionsLocator: Locator;
+    quickLaunchLocator: Locator;
+    buzzLatestPostsLocator: Locator;
+    empOnLeaveLocator: Locator;
+    empDistrBySubLocator: Locator;
+    empDistrByLocLocator: Locator;
+    pimLocator: Locator;
+
+    constructor(public page: Page) {
+        this.timeAtWorkLocator = this.page.locator(".oxd-sheet").nth(1);
+        this.myActionsLocator = this.page.locator(".oxd-sheet").nth(2);
+        this.quickLaunchLocator = this.page.locator(".oxd-sheet").nth(3);
+        this.buzzLatestPostsLocator = this.page.locator(".oxd-sheet").nth(4);
+        this.empOnLeaveLocator = this.page.locator(".oxd-sheet").nth(5);
+        this.empDistrBySubLocator = this.page.locator(".oxd-sheet").nth(6);
+        this.empDistrByLocLocator = this.page.locator(".oxd-sheet").last();
+        this.pimLocator = this.page.getByRole("link", {name: "PIM"});
+    }
+
+    async verifyDashboardPageElements() {
+        await expect(this.timeAtWorkLocator).toBeVisible();
+        await expect(this.myActionsLocator).toBeVisible();
+        await expect(this.quickLaunchLocator).toBeVisible();
+        await expect(this.buzzLatestPostsLocator).toBeVisible();
+        await expect(this.empOnLeaveLocator).toBeVisible();
+        await expect(this.empDistrBySubLocator).toBeVisible();
+        await expect(this.empDistrByLocLocator).toBeVisible();
+    }
+
+    async goToPIM() {
+        await this.pimLocator.click();
+        await expect(this.page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList");
+    }
+
+}
--- tests/pom-tests/add-employee-flow.spec.ts
+++ tests/pom-tests/add-employee-flow.spec.ts
@@ -1,9 +1,12 @@
 import {test} from '@playwright/test';
 import {LoginPage} from '../../pages/LoginPage';
+import {DashboardPage} from '../../pages/DashboardPage';
 
 test("Add Employee Flow", async ({page}) => {
     const loginPage = new LoginPage(page);
-
+    const dashboardPage = new DashboardPage(page);
     await loginPage.navigateToHomePage();
     await loginPage.login("Admin", "admin123");
+    await dashboardPage.verifyDashboardPageElements();
+    await dashboardPage.goToPIM();
 })
