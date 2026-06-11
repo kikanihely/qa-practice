e9188f0deac539eb8557f153adc4d617967c7ace
Kikani Hely <helykikani2020@gmail.com>
2026-06-11 16:34:30 +0530

    Data driven assign leave flow added

--- pages/DashboardPage.ts
+++ pages/DashboardPage.ts
@@ -13,11 +13,11 @@ export class DashboardPage {
     assignLeaveLocator: Locator;
 
     constructor(public page: Page) {
-        this.timeAtWorkLocator = this.page.locator(".oxd-sheet").nth(0);
-        this.myActionsLocator = this.page.locator(".oxd-sheet").nth(1);
-        this.quickLaunchLocator = this.page.locator(".oxd-sheet").nth(2);
-        this.buzzLatestPostsLocator = this.page.locator(".oxd-sheet").nth(3);
-        this.empOnLeaveLocator = this.page.locator(".oxd-sheet").nth(4);
+        this.timeAtWorkLocator      = this.page.getByText('Time at Work', { exact: true })
+    this.myActionsLocator       = this.page.getByText('My Actions', { exact: true })
+    this.quickLaunchLocator     = this.page.getByText('Quick Launch', { exact: true })
+    this.buzzLatestPostsLocator = this.page.getByText('Buzz Latest Posts', { exact: true })
+    this.empOnLeaveLocator      = this.page.getByText('Employees on Leave Today', { exact: true })
         this.empDistrBySubLocator = this.page.locator(".oxd-sheet").nth(5);
         this.empDistrByLocLocator = this.page.locator(".oxd-sheet").last();
         this.pimLocator = this.page.getByRole("link", {name: "PIM"});
@@ -29,11 +29,11 @@ export class DashboardPage {
     }
 
     async verifyDashboardPageElements() {
-        await expect(this.timeAtWorkLocator).toBeVisible();
-        await expect(this.myActionsLocator).toBeVisible();
-        await expect(this.quickLaunchLocator).toBeVisible();
-        await expect(this.buzzLatestPostsLocator).toBeVisible();
-        await expect(this.empOnLeaveLocator).toBeVisible();
+        await expect(this.timeAtWorkLocator).toBeVisible({ timeout: 10000 })
+    await expect(this.myActionsLocator).toBeVisible({ timeout: 10000 })
+    await expect(this.quickLaunchLocator).toBeVisible({ timeout: 10000 })
+    await expect(this.buzzLatestPostsLocator).toBeVisible({ timeout: 10000 })
+    await expect(this.empOnLeaveLocator).toBeVisible({ timeout: 10000 })
         await expect(this.empDistrBySubLocator).toBeVisible();
         await expect(this.empDistrByLocLocator).toBeVisible();
     }
