37fe373ef7bfbb2ed43e922cd1eb3c9873a81ca5
Kikani Hely <helykikani2020@gmail.com>
2026-06-12 11:52:35 +0530

    Data driven assign leave flow added

--- tests/pom-tests/add-employee-flow.spec.ts
+++ tests/pom-tests/add-employee-flow.spec.ts
@@ -21,18 +21,18 @@ test("Add Employee Flow", async ({page}) => {
     await dashboardPage.gotoDashboard()
     await dashboardPage.goToPIM();
     await employeeListPage.clickAddEmployee();
-    await employeeAddPage.fillEmployeeDetails(data.employee.firstName[3], data.employee.middleName[3], data.employee.lastName[3], data.employee.employeeId[3]);
-    await employeeAddPage.fillLoginDetails(data.employee.username[3], data.employee.password[3]);
+    await employeeAddPage.fillEmployeeDetails(data.employee.firstName[4], data.employee.middleName[4], data.employee.lastName[4], data.employee.employeeId[4]);
+    await employeeAddPage.fillLoginDetails(data.employee.username[4], data.employee.password[4]);
     // await employeeAddPage.validatePage();
     // await page.waitForURL('**/pim/viewPersonalDetails/empNumber/**')
-    await employeeDetailPage.fillDriverLicense(data.employee.driverLicenseNumber[3])
-    await employeeDetailPage.fillExpiryDate(data.employee.licenseExpiryDate[3])
-    await employeeDetailPage.selectNationality(data.employee.nationality[3])
-    await employeeDetailPage.selectMaritalStatus(data.employee.maritalStatus[3])
-    await employeeDetailPage.selectGender(data.employee.gender[3] as 'Male' | 'Female')
+    await employeeDetailPage.fillDriverLicense(data.employee.driverLicenseNumber[4])
+    await employeeDetailPage.fillExpiryDate(data.employee.licenseExpiryDate[4])
+    await employeeDetailPage.selectNationality(data.employee.nationality[4])
+    await employeeDetailPage.selectMaritalStatus(data.employee.maritalStatus[4])
+    await employeeDetailPage.selectGender(data.employee.gender[4] as 'Male' | 'Female')
     await employeeDetailPage.saveDetails()
     await page.waitForLoadState('networkidle')
     await dashboardPage.goToPIM()
-    await employeeListPage.searchEmployee("demoo")
+    await employeeListPage.searchEmployee(data.employee.firstName[4])
 
 })
