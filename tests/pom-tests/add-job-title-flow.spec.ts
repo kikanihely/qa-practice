6a8d4bab6a3ed08de9cebaac507bd8f6b8e0f443
Kikani Hely <helykikani2020@gmail.com>
2026-06-12 17:29:28 +0530

    Data driven add job title flow added

--- tests/pom-tests/add-job-title-flow.spec.ts
+++ tests/pom-tests/add-job-title-flow.spec.ts
@@ -14,7 +14,7 @@ test("Ass Job Title", async({page}) => {
     await dashboardPage.goToAdminPage();
     await adminPage.goToJobTitle();
     await jobListPage.goToAddJob();
-    await addJobTitlePage.fillJobDetails(data.job[6].title,data.job[6].description,data.job[6].note);
+    await addJobTitlePage.fillJobDetails(data.job[4].title,data.job[4].description,data.job[4].note);
     await addJobTitlePage.saveDetails();
-    await jobListPage.verifyJob(data.job[6].title);
+    await jobListPage.verifyJob(data.job[4].title);
 })
